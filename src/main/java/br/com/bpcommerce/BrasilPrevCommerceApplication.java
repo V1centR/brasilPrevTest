636640d3311af80c33fe5761d18345bc4c8945b3
Dev <vicentcdb@gmail.com>
2020-06-27 21:04:01 -0300

    setup project

---
+++ src/main/java/br/com/bpcommerce/BrasilPrevCommerceApplication.java
@@ -0,0 +1,12 @@
+package br.com.bpcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasilPrevCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasilPrevCommerceApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/bpcommerce/BrasilPrevCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bpcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasilPrevCommerceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
